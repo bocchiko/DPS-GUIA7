f7d99fc79bc8c13a175da57e1b9662b85e9f6b25
bocchiko <124472850+bocchiko@users.noreply.github.com>
2023-09-27 17:51:53 -0600

    Dependecies & Images

--- viajes/App.js
+++ viajes/App.js
@@ -1,21 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const App = () => {
+  return(
+    <>
+    <ScrollView>
+      <View style={{flexDirection: 'row'}}>
+        <Image 
+          style={styles.banner}
+          source={require('./src/img/bg.jpg')}
+          />
+      </View>
+
+      <View style={styles.contenedor}>
+        <Text style={styles.titulo}>Que hacer en El Salvador</Text>
+        <ScrollView horizontal>
+          <View>
+            <Image 
+              style={styles.ciudad}
+              source={require('./src/img/actividad1.jpg')}
+            />
+          </View>
+
+          <View>
+            <Image 
+              style={styles.ciudad}
+              source={require('./src/img/actividad2.jpg')}
+            />
+          </View>
+
+          <View>
+            <Image 
+              style={styles.ciudad}
+              source={require('./src/img/actividad3.jpg')}
+            />
+          </View>
+
+          <View>
+            <Image 
+              style={styles.ciudad}
+              source={require('./src/img/actividad4.jpg')}
+            />
+          </View>
+
+          <View>
+            <Image 
+              style={styles.ciudad}
+              source={require('./src/img/actividad5.jpg')}
+            />
+          </View>
+        </ScrollView>
+      </View>
+
+    </ScrollView>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  banner: {
+    height: 250,
+    flex: 1
   },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 10
+  },
+  contenedor: {
+    marginHorizontal: 10
+  },
+  ciudad: {
+    width: 250,
+    height: 300,
+    marginRight: 10
+  }
 });
+
+export default App;
