82c30845006cf77869a91327c5d23d7543b79656
bocchiko <124472850+bocchiko@users.noreply.github.com>
2023-09-27 18:25:26 -0600

    Doc Format

--- viajes/App.js
+++ viajes/App.js
@@ -17,125 +17,125 @@ export default function App() {
   const [modalVisibleplaya, setModalVisibleplaya] = React.useState(false);
 
   return (
-    
-      <ScrollView>
-        <Modal
-          transparent={true}
-          visible={modalVisibleplaya}
-          animationType="slide"
-          onRequestClose={() => {
-            alert("Modal has been closed.");
-          }}
-        >
-          <View style={styles.vistaModal}>
-            <View style={styles.Modal}>
-              <Text style={styles.subtitulo}>Ir a la playa</Text>
-              <Text>
-                El Salvador cuenta con hermosas playas a nivel centroamericano.
-              </Text>
-              <Button
-                title="Cerrar"
-                onPress={() => {
-                  setModalVisibleplaya(!modalVisibleplaya);
-                }}
-              ></Button>
-            </View>
+
+    <ScrollView>
+      <Modal
+        transparent={true}
+        visible={modalVisibleplaya}
+        animationType="slide"
+        onRequestClose={() => {
+          alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.vistaModal}>
+          <View style={styles.Modal}>
+            <Text style={styles.subtitulo}>Ir a la playa</Text>
+            <Text>
+              El Salvador cuenta con hermosas playas a nivel centroamericano.
+            </Text>
+            <Button
+              title="Cerrar"
+              onPress={() => {
+                setModalVisibleplaya(!modalVisibleplaya);
+              }}
+            ></Button>
           </View>
-        </Modal>
-        <View style={{ flexDirection: "row" }}>
-          <Image style={styles.banner} source={require("./img/bg.jpg")} />
         </View>
-        <View style={styles.contenedor}>
-          <Text style={styles.titulo}>Que hacer en El Salvador</Text>
-          <ScrollView horizontal>
-            <View>
-              <TouchableHighlight
-                onPress={() => {
-                  setModalVisibleplaya(!modalVisibleplaya);
-                }}
-              >
-                <Image
-                  style={styles.ciudad}
-                  source={require("./img/actividad1.jpg")}
-                />
-              </TouchableHighlight>
-            </View>
-            <View>
-              <Image
-                style={styles.ciudad}
-                source={require("./img/actividad2.jpg")}
-              />
-            </View>
-            <View>
-              <Image
-                style={styles.ciudad}
-                source={require("./img/actividad3.jpg")}
-              />
-            </View>
-            <View>
-              <Image
-                style={styles.ciudad}
-                source={require("./img/actividad4.jpg")}
-              />
-            </View>
-            <View>
-              <Image
-                style={styles.ciudad}
-                source={require("./img/actividad5.jpg")}
-              />
-            </View>
-          </ScrollView>
-          <Text style={styles.titulo}>Platillos Salvadoreños</Text>
+      </Modal>
+      <View style={{ flexDirection: "row" }}>
+        <Image style={styles.banner} source={require("./img/bg.jpg")} />
+      </View>
+      <View style={styles.contenedor}>
+        <Text style={styles.titulo}>Que hacer en El Salvador</Text>
+        <ScrollView horizontal>
           <View>
-            <View>
+            <TouchableHighlight
+              onPress={() => {
+                setModalVisibleplaya(!modalVisibleplaya);
+              }}
+            >
+              <Image
+                style={styles.ciudad}
+                source={require("./img/actividad1.jpg")}
+              />
+            </TouchableHighlight>
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require("./img/actividad2.jpg")}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require("./img/actividad3.jpg")}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require("./img/actividad4.jpg")}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require("./img/actividad5.jpg")}
+            />
+          </View>
+        </ScrollView>
+        <Text style={styles.titulo}>Platillos Salvadoreños</Text>
+        <View>
+          <View>
+            <Image
+              style={styles.mejores}
+              source={require("./img/mejores1.jpg")}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.mejores}
+              source={require("./img/mejores2.jpg")}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.mejores}
+              source={require("./img/mejores3.jpg")}
+            />
+          </View>
+          <Text style={styles.titulo}>Rutas Turisticas</Text>
+          <View style={styles.listado}>
+            <View style={styles.listaItem}>
               <Image
                 style={styles.mejores}
-                source={require("./img/mejores1.jpg")}
+                source={require("./img/ruta1.jpg")}
               />
             </View>
-            <View>
+            <View style={styles.listaItem}>
               <Image
                 style={styles.mejores}
-                source={require("./img/mejores2.jpg")}
+                source={require("./img/ruta2.jpg")}
               />
             </View>
-            <View>
+            <View style={styles.listaItem}>
               <Image
                 style={styles.mejores}
-                source={require("./img/mejores3.jpg")}
+                source={require("./img/ruta3.jpg")}
               />
             </View>
-            <Text style={styles.titulo}>Rutas Turisticas</Text>
-            <View style={styles.listado}>
-              <View style={styles.listaItem}>
-                <Image
-                  style={styles.mejores}
-                  source={require("./img/ruta1.jpg")}
-                />
-              </View>
-              <View style={styles.listaItem}>
-                <Image
-                  style={styles.mejores}
-                  source={require("./img/ruta2.jpg")}
-                />
-              </View>
-              <View style={styles.listaItem}>
-                <Image
-                  style={styles.mejores}
-                  source={require("./img/ruta3.jpg")}
-                />
-              </View>
-              <View style={styles.listaItem}>
-                <Image
-                  style={styles.mejores}
-                  source={require("./img/ruta4.jpg")}
-                />
-              </View>
+            <View style={styles.listaItem}>
+              <Image
+                style={styles.mejores}
+                source={require("./img/ruta4.jpg")}
+              />
             </View>
           </View>
         </View>
-      </ScrollView>
-    
+      </View>
+    </ScrollView>
+
   );
 }
 
